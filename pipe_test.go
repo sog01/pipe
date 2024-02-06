94b9dfb2c54df9823720099dbcbae21764c95272
sog01 <abdullahsog3@gmail.com>
2024-02-07 04:57:04 +0700

    test: add pipe and pipeGo combination unit test

--- pipe_test.go
+++ pipe_test.go
@@ -1,6 +1,7 @@
 package pipe
 
 import (
+	"errors"
 	"fmt"
 	"testing"
 	"time"
@@ -120,6 +121,112 @@ func TestPipeGo(t *testing.T) {
 				return res
 			}, map[string]struct{}{})
 			assert.Equal(t, tt.want, gotMap)
+		})
+	}
+}
+
+func TestPipeAndPipeGo(t *testing.T) {
+	type createUserRequest struct {
+		Username          string
+		Email             string
+		ProfilePictureUrl string
+	}
+
+	type args struct {
+		args  createUserRequest
+		funcs []Func[createUserRequest]
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr error
+	}{
+		{
+			name: "create user",
+			args: args{
+				args: createUserRequest{
+					Username:          "username",
+					Email:             "email",
+					ProfilePictureUrl: "profilePictureUrl",
+				},
+				funcs: []Func[createUserRequest]{
+					func(args createUserRequest, responses []any) (response any, err error) {
+						return args.Username, nil
+					},
+					func(args createUserRequest, responses []any) (response any, err error) {
+						username := responses[0].(string)
+						fmt.Println(username)
+						return args.Email, nil
+					},
+					func(args createUserRequest, responses []any) (response any, err error) {
+						email := responses[1].(string)
+						fmt.Println(email)
+						return args.ProfilePictureUrl, nil
+					},
+					PipeGo(
+						func(args createUserRequest, responses []any) (response any, err error) {
+							time.Sleep(1 * time.Second)
+							return "go" + args.Username, nil
+						},
+						func(args createUserRequest, responses []any) (response any, err error) {
+							time.Sleep(1 * time.Second)
+							return "go" + args.Email, nil
+						},
+						func(args createUserRequest, responses []any) (response any, err error) {
+							time.Sleep(1 * time.Second)
+							return "go" + args.ProfilePictureUrl, nil
+						},
+					),
+				},
+			},
+			wantErr: nil,
+		},
+		{
+			name: "create user error",
+			args: args{
+				args: createUserRequest{
+					Username:          "username",
+					Email:             "email",
+					ProfilePictureUrl: "profilePictureUrl",
+				},
+				funcs: []Func[createUserRequest]{
+					func(args createUserRequest, responses []any) (response any, err error) {
+						return args.Username, nil
+					},
+					func(args createUserRequest, responses []any) (response any, err error) {
+						username := responses[0].(string)
+						fmt.Println(username)
+						return args.Email, nil
+					},
+					func(args createUserRequest, responses []any) (response any, err error) {
+						email := responses[1].(string)
+						fmt.Println(email)
+						return args.ProfilePictureUrl, nil
+					},
+					PipeGo(
+						func(args createUserRequest, responses []any) (response any, err error) {
+							time.Sleep(1 * time.Second)
+							return "go" + args.Username, errors.New("errors go username")
+						},
+						func(args createUserRequest, responses []any) (response any, err error) {
+							time.Sleep(1 * time.Second)
+							return "go" + args.Email, nil
+						},
+						func(args createUserRequest, responses []any) (response any, err error) {
+							time.Sleep(1 * time.Second)
+							return "go" + args.ProfilePictureUrl, nil
+						},
+					),
+				},
+			},
+			wantErr: errors.New("errors go username"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			exec := Pipe(tt.args.funcs...)
+			_, err := exec(tt.args.args, nil)
+			assert.Equal(t, tt.wantErr, err)
 		})
 	}
 }
